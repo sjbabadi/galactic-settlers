9f48a7885a0ab91be62c2752f12b5565f4445d42
sjbabadi <sheila.babadi@yahoo.com>
2019-02-08 11:27:59 -0600

    add camera pan and zoom with mouse

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    //way to keep track of types of buildings
+    public int id;
+    public int cost;
+    public string BuildingName;
+}
