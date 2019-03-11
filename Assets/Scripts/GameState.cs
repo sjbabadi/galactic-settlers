08fa111f03bbae45c02598055abbe20ac9142850
sjbabadi <sheila.babadi@yahoo.com>
2019-03-11 09:43:54 -0700

    update GameState script with resource calculations

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
-    public int Turn { get; set;  }
+    [SerializeField] public int Turn { get; set; }
 
     public int Money { get; set; }
 
@@ -16,18 +16,57 @@ public class GameState : MonoBehaviour
 
     public int Units { get; set; }
 
+    public int UnitMax { get; set; }
+
     public int MaxPop { get; set; }
+
+    public enum Buildings { Farm, Mine, Barracks };
+
+    public int[] buildingCounts = new int[3];
 
     private HUDController HUDController;
 
     private void Start()
     {
         HUDController = FindObjectOfType<HUDController>();
+        buildingCounts[(int)Buildings.Farm] = 0;
+        buildingCounts[(int)Buildings.Mine] = 0;
+        buildingCounts[(int)Buildings.Barracks] = 0;
+
+        Money = 100;
+        Population = 0;
+        BaseHealth = 100;
+        Units = 0;
+        MaxPop = 5;
+        UnitMax = 0;
+        Turn = 0;
     }
 
     public void EndTurn()
     {
         Turn++;
-        //HUDController.UpdateStats();
+        HUDController.UpdateStats();
     }
+
+    void CalculateFood()
+    {
+        Food += buildingCounts[(int)Buildings.Farm];
+    }
+
+    void CalculateMoney()
+    {
+        Money += 25 * buildingCounts[(int)Buildings.Mine]; // 25 money per turn per mine
+    }
+
+    void CalculateMaxPop()
+    {
+        MaxPop = 5 * buildingCounts[(int)Buildings.Farm]; // 5 ppl allowed per farm
+    }
+
+    void CalculateUnitMax()
+    {
+        UnitMax = 5 * buildingCounts[(int)Buildings.Barracks];
+    }
+
+
 }
