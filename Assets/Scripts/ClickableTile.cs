6849b62cca101a79fb2069c94226ea5c639cd983
tdekalb <tdekalb@ufl.edu>
2019-04-10 17:35:07 -0400

    test merge

---
+++ Assets/Scripts/ClickableTile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableTile : MonoBehaviour
+{
+
+    public int tileX;
+    public int tileY;
+    public Tile_map map;
+
+    void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0) && map.selectedUnit != null)
+        {
+            Debug.Log("Click!");
+
+            map.GeneratePathTo(tileX, tileY);
+
+            while (map.selectedUnit.GetComponent<Unit>().currentPath != null)
+            {
+                map.selectedUnit.GetComponent<UnitController>().MoveNextTile();
+            }
+
+            map.selectedUnit = null;
+        }
+    }
+}
