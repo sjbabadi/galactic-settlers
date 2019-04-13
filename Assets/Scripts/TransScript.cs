010c637a5933378fbb59e35fe7e8abac27ba8228
Luis Ruiz <31607146+egm42@users.noreply.github.com>
2019-04-13 02:19:08 -0400

    Implemented building restrictions
    Building are only able to be built if the tile is in the playerBuildingList list.

--- Assets/Scripts/TransScript.cs
+++ Assets/Scripts/TransScript.cs
@@ -22,9 +22,10 @@ public class TransScript : MonoBehaviour
         {
             Vector2 mouseRay = Camera.main.ScreenToWorldPoint(transform.position);
             RaycastHit2D rayHit = Physics2D.Raycast(mousePos, Vector2.zero, Mathf.Infinity, buildingLayer);
-            Debug.Log(rayHit.collider.name);
+
+            Tile tile = rayHit.collider.GetComponent<Tile>();
             //if we don't hit another building, build here
-            if (rayHit.collider.gameObject.GetComponent<Tile>().walkable && rayHit.collider.gameObject.GetComponent<Tile>().empty)
+            if (tile.walkable && tile.empty && FindObjectOfType<GameState>().playerBuildingLocations.Contains(tile))
             {
                 Instantiate(placeObject, transform.position, Quaternion.identity);
                 PlacementScript.ClearSelection();
